fd7a0a0e3b85935b2a3c608004fb2dde9e33c206
tmdan <mukhammed.ne@gmail.com>
2021-08-25 08:57:07 +0300

    added method exists checker for different response objects

--- src/Middleware/ApiLoggerMiddleware.php
+++ src/Middleware/ApiLoggerMiddleware.php
@@ -40,15 +40,15 @@ class ApiLoggerMiddleware
         if ($request->route()->getName() != "api.logger" && $this->enabled && $request->wantsJson()) {
 
             ApiLogger::create([
-                'request_full_url' => $request->fullUrl()?? null,
-                'request_method' => $request->method()?? null,
-                'request_body' => $request->all()?? null,
-                'request_ip' => $request->ip()?? null,
-                'request_header' => $request->header()?? null,
-                'request_agent' => $request->header('User-Agent'),
-                'response_content' => $response->content() ?? null,
-                'response_status_code' => $response->getStatusCode()?? null,
-                'user_id' => $request->user()->id ?? null,
+                'request_full_url' => method_exists($request, 'fullUrl') ? $request->fullUrl() : null,
+                'request_method' => method_exists($request, 'method') ? $request->method() : null,
+                'request_body' => method_exists($request, 'all') ? $request->all() : null,
+                'request_ip' => method_exists($request, 'ip') ? $request->ip() : null,
+                'request_header' => method_exists($request, 'header') ? $request->header() : null,
+                'request_agent' => method_exists($request, 'header') ? $request->header('User-Agent') : null,
+                'response_content' => method_exists($response, 'content') ? $response->content() : null,
+                'response_status_code' => method_exists($response, 'getStatusCode') ? $response->getStatusCode() : null,
+                'user_id' =>$request->user()->id ?? null,
                 'user_timezone' => $request->user()->timezone ?? null,
             ]);
 
